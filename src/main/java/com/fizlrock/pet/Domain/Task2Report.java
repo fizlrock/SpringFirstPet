8f7d3e4529d0a2afde762b223f76b9864fa7aefd
fizlrock <nirku0soft@yandex.ru>
2024-04-03 15:59:40 +0400

    Структуры данных для хранения отчета по заданию 2

--- src/main/java/com/fizlrock/pet/Domain/Task2Report.java
+++ src/main/java/com/fizlrock/pet/Domain/Task2Report.java
@@ -1,37 +1,43 @@
 package com.fizlrock.pet.Domain;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 
 /**
- * Task2Report
  * Хранит:
  * <ul>
  * <li>Кол-во бит в исходном сообщении
  * <li>Кол-во бит в закодированном сообщении
  * <li>Номера контрольных бит
  * <li>Номера информацонных бит
- * <li>Список шагов кодирования. 
+ * <li>Список шагов кодирования.
  * </ul>
  */
-public class Task2Report {
+@Builder
+public record Task2Report(
+    int dataSize,
+    int cryptedDataSize,
+    int[] dataBits,
+    int[] controlBits,
+    List<CodingStep> steps) {
 
   /**
    * Шаг кодирования содержит:
    * <ul>
-   * <li>Номер шага (текущего расчитываемого контрольного бита) 
-   * <li>Текущее состояния буффера 
+   * <li>Номер шага (текущего расчитываемого контрольного бита)
+   * <li>Текущее состояния буффера
    * <li>Номера бит, используемых для расчета
    * <li>Краткий коментарий (1 + 0 + 1... = 34)
    * <li>Результат расчета (контрольного бита)
    * </ul>
    */
-  @AllArgsConstructor
-  public static class CodingStep {
-    private final int stepNum;
-    private final int[] bufferState;
-    private final int[] usedBits;
-    private final String comment;
-
-  }
-
+  @Builder
+  public static record CodingStep(
+      int stepNum,
+      int[] bufferState,
+      int[] usedBits,
+      String comment) {
+  };
 }
